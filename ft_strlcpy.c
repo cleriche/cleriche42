e75e4a489b0ead0f8ede7cd1fb8b67f576d95607
cleriche <christophe.lrch@gmail.com>
2024-11-05 16:45:48 +0100

    Add files via upload

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,21 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cleriche <cleriche@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 12:31:13 by cleriche          #+#    #+#             */
-/*   Updated: 2024/11/05 15:54:52 by cleriche         ###   ########.fr       */
+/*   Created: 2024/11/05 10:58:24 by cleriche          #+#    #+#             */
+/*   Updated: 2024/11/05 12:58:03 by cleriche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t	ft_strlen(const char *str)
-{
-	int	i;
+#include "libft.h"
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	if (!dest || !src)
+		return (0);
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
 	i = 0;
-	while (str[i])
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
 		i++;
-	return (i);
+	}
+	dest[i] = '\0';
+	return (src_len);
 }
